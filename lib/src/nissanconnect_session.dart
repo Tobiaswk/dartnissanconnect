7413645ab10a6c9b63619743acdb307ba7ae6d8b
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2020-01-24 22:50:33 +0100

    Tidying up

--- lib/src/nissanconnect_session.dart
+++ lib/src/nissanconnect_session.dart
@@ -5,7 +5,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class NissanConnectSession {
-    Map settings = <String, Map>{
+  Map settings = <String, Map>{
     "EU": <String, String>{
       "client_id": "a-ncb-prod-android",
       "client_secret":
@@ -23,8 +23,9 @@ class NissanConnectSession {
   };
 
   var API_VERSION = "protocol=1.0,resource=2.1";
-  var SRP_KEY = "D5AF0E14718E662D12DBB4FE42304DF5A8E48359E22261138B40AA16CC85C76A11B43200A1EECB3C9546A262D1FBD51ACE6FCDE558C00665BBF93FF86B9F8F76AA7A53CA74F5B4DFF9A4B847295E7D82450A2078B5A28814A7A07F8BBDD34F8EEB42B0E70499087A242AA2C5BA9513C8F9D35A81B33A121EEF0A71F3F9071CCD";
-  
+  var SRP_KEY =
+      "D5AF0E14718E662D12DBB4FE42304DF5A8E48359E22261138B40AA16CC85C76A11B43200A1EECB3C9546A262D1FBD51ACE6FCDE558C00665BBF93FF86B9F8F76AA7A53CA74F5B4DFF9A4B847295E7D82450A2078B5A28814A7A07F8BBDD34F8EEB42B0E70499087A242AA2C5BA9513C8F9D35A81B33A121EEF0A71F3F9071CCD";
+
   bool debug;
   List<String> debugLog = List<String>();
 
@@ -37,10 +38,11 @@ class NissanConnectSession {
 
   NissanConnectSession({this.debug = false});
 
-  Future<NissanConnectResponse> requestWithRetry({String endpoint,
-    String method = "POST",
-    Map additionalHeaders,
-    Map params}) async {
+  Future<NissanConnectResponse> requestWithRetry(
+      {String endpoint,
+      String method = "POST",
+      Map additionalHeaders,
+      Map params}) async {
     NissanConnectResponse response = await request(
         endpoint: endpoint,
         method: method,
@@ -97,7 +99,7 @@ class NissanConnectSession {
     dynamic jsonData;
     try {
       jsonData = json.decode(response.body);
-      _print('result: $jsonData');
+      _print('Result: $jsonData');
     } catch (e) {
       _print('JSON decoding failed!');
     }
@@ -111,26 +113,29 @@ class NissanConnectSession {
     this.password = password;
     this.bearerToken = null;
 
-    Map<String, String> headers = Map();
-    headers["Accept-Api-Version"] = API_VERSION;
-    headers["X-Username"] = "anonymous";
-    headers["X-Password"] = "anonymous";
-    headers["Content-Type"] = "application/json";
-    headers["Accept"] = "application/json";
-
     NissanConnectResponse response = await request(
         endpoint:
             "${settings["EU"]["auth_base_url"]}json/realms/root/realms/${settings["EU"]["realm"]}/authenticate",
-        additionalHeaders: headers);
+        additionalHeaders: <String, String>{
+          "Accept-Api-Version": API_VERSION,
+          "X-Username": "anonymous",
+          "X-Password": "anonymous",
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        });
 
     var authId = response.body["authId"];
-
-    //headers['Cookie'] = response.headers['set-cookie'];
 
     response = await request(
         endpoint:
             "${settings["EU"]["auth_base_url"]}json/realms/root/realms/${settings["EU"]["realm"]}/authenticate",
-        additionalHeaders: headers,
+        additionalHeaders: <String, String>{
+          "Accept-Api-Version": API_VERSION,
+          "X-Username": "anonymous",
+          "X-Password": "anonymous",
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
         params: {
           "authId": authId,
           "template": "",
@@ -160,10 +165,6 @@ class NissanConnectSession {
 
     var authCookie = response.body['tokenId'];
 
-    headers = Map<String, String>();
-    headers["Cookie"] =
-        "i18next=en-UK; amlbcookie=05; kauthSession=\"$authCookie\"";
-
     // Extremely dirty
     // The http client throws an error due to an invalid URI from the API
     // We parse the code used for authentication from the error message
@@ -172,35 +173,34 @@ class NissanConnectSession {
       response = await request(
           endpoint:
               "${settings["EU"]["auth_base_url"]}oauth2${response.body["realm"]}/authorize?client_id=${settings["EU"]["client_id"]}&redirect_uri=${settings["EU"]["redirect_uri"]}&response_type=code&scope=${settings["EU"]["scope"]}&nonce=sdfdsfez",
-          additionalHeaders: headers,
+          additionalHeaders: <String, String>{
+            "Cookie":
+                "i18next=en-UK; amlbcookie=05; kauthSession=\"$authCookie\""
+          },
           method: 'GET');
-    print(response.body);
+      print(response.body);
     } catch (e) {
       code = e.message.split("=")[1].split('&')[0];
     }
-	
-
-    headers = Map<String, String>();
-    headers["Content-Type"] = 'application/x-www-form-urlencoded';
 
     response = await request(
       endpoint:
           "${settings["EU"]["auth_base_url"]}oauth2${response.body["realm"]}/access_token?code=$code&client_id=${settings["EU"]["client_id"]}&client_secret=${settings["EU"]["client_secret"]}&redirect_uri=${settings["EU"]["redirect_uri"]}&grant_type=authorization_code",
-      additionalHeaders: headers,
+      additionalHeaders: <String, String>{
+        "Content-Type": "application/x-www-form-urlencoded"
+      },
     );
 
     this.bearerToken = response.body['access_token'];
 
     response = await request(
-        endpoint:
-        "${settings["EU"]["user_adapter_base_url"]}v1/users/current",
+        endpoint: "${settings["EU"]["user_adapter_base_url"]}v1/users/current",
         method: 'GET');
 
     var userId = response.body['userId'];
 
     response = await request(
-        endpoint:
-        "${settings["EU"]["user_base_url"]}v2/users/$userId/cars",
+        endpoint: "${settings["EU"]["user_base_url"]}v2/users/$userId/cars",
         method: 'GET');
 
     vehicles = List<NissanConnectVehicle>();
