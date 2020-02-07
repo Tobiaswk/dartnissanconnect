6c87fc0b9146fc31713cd8b0654de2b119365bb7
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2020-02-07 18:31:55 +0100

    Last 30 days as period for monthly statistics

--- lib/src/nissanconnect_vehicle.dart
+++ lib/src/nissanconnect_vehicle.dart
@@ -46,7 +46,7 @@ class NissanConnectVehicle {
   }
 
   Future<NissanConnectStats> requestMonthlyStatistics() async {
-    var start = DateTime(DateTime.now().year, DateTime.now().month, 1);
+    var start = DateTime.now().subtract(new Duration(days: 30));
     var end = DateTime.now();
     var response = await session.requestWithRetry(
         endpoint:
